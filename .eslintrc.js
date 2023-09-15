8c9e414befa6c4a8a0cde94d490872396550da2a
takumi <takumi.kimura@brachio.co.jp>
2023-09-15 13:18:10 +0900

    eslint-plugin-unused-imports導入とimport/order

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,42 @@
 module.exports = {
   root: true,
   extends: ["plugin:@typescript-eslint/recommended", "next/core-web-vitals"],
-  plugins: [],
+  plugins: ["unused-imports"],
   parserOptions: {
     project: "./tsconfig.json",
   },
   rules: {
-    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-explicit-any": "warn",
     "@typescript-eslint/no-unsafe-call": "error",
     "@typescript-eslint/no-unsafe-member-access": "error",
     "@typescript-eslint/no-unsafe-return": "error",
+    "unused-imports/no-unused-imports-ts": "warn",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+        pathGroups: [
+          {
+            pattern: "{react,react-dom/**,react-router-dom}",
+            group: "builtin",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: {
+          order: "asc",
+        },
+      },
+    ],
   },
 };
